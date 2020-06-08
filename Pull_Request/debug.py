20d6b263086e686375dfadb684bb4f2e6a946831
DeadlySlav <65268111+DeadlySlav@users.noreply.github.com>
2020-06-08 14:59:20 -0700

    Update debug.py
    Hey Dylan, try this code out and let me know if it works!

--- Pull_Request/debug.py
+++ Pull_Request/debug.py
@@ -1,1 +1,15 @@
-
+a = int(input())
+b = int(input())
+c = int(input())
+def lone_sum(a, b, c):
+    if not (not (a == b) or not (a == c) or not (b == c)):
+        return 0
+    elif a >= b:
+        return c
+    elif a == c:
+        return b
+    elif b == c:
+        return a
+    else:
+        return a+b+c
+print(lone_sum(a, b, c))
